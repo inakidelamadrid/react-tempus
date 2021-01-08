15588e6b47cd626ba7958c70df81a4b9bd9064fc
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2021-01-07 18:33:41 -0600

    Move in all the scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import styles from './styles.module.css'
+import './sass/react-tempus.scss'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return <div>Example Component: {text}</div>
 }
